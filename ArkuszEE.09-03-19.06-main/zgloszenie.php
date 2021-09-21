b7f7fefb44f3e3a725102182d3980119f8a8d551
Jelonidas <72608651+Jelonidas@users.noreply.github.com>
2021-09-21 13:18:59 +0200

    Add files via upload

---
+++ ArkuszEE.09-03-19.06-main/zgloszenie.php
@@ -0,0 +1,13 @@
+<?php
+$lowisko = $_POST['lowisko'];
+$data = $_POST['data'];
+$sedzia = $_POST['sedzia'];
+$baza=mysqli_connect('localhost','root','','wedkarstwo');
+
+if(mysqli_connect_errno())
+{echo"wystapil blad polaczenia z baza";}
+$wynik=mysqli_query($baza, "INSERT INTO `zawody_wedkarskie`(`id`, `Karty_wedkarskie_id`, `Lowisko_id`, `data_zawodow`, `sedzia`) VALUES 
+(' ', 0,'$lowisko','$data','$sedzia' )");
+
+mysqli_close($baza);
+?>
